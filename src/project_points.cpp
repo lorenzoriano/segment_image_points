738b2d8a3bde5b2cbd9740e2d476f88e84e2c888
lorenzoriano <lorenzo.riano@berkeley.edu>
2013-02-20 15:57:39 -0800

    initial commit

---
+++ src/project_points.cpp
@@ -0,0 +1,127 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/image_encodings.h>
+#include <pcl/point_types.h>
+#include <pcl/ros/conversions.h>
+#include <pcl_ros/transforms.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_eigen.h>
+#include <image_transport/image_transport.h>
+#include <opencv/cv.h>
+#include <cv_bridge/cv_bridge.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+class PointCloudProjector {
+
+public:
+    PointCloudProjector() : nh_(), it_(nh_) {
+        pc_sub_ = nh_.subscribe("/camera/depth_registered/points", 1, &PointCloudProjector::cloud_cb, this);
+        camerasub_ = it_.subscribeCamera("/camera/rgb/image_rect_color", 1, &PointCloudProjector::image_cb, this);
+        imagepub_ = it_.advertise("image_out", 1);
+
+        newimage_ = false;
+    }
+
+    void cloud_cb(const sensor_msgs::PointCloud2& input) {
+
+        if (last_image_.empty()) {
+            ROS_WARN("I don't have an image yet");
+            return;
+        }
+        if (! newimage_)
+            return;
+        newimage_ = false;
+
+        tf::StampedTransform transform;
+        try {
+            ros::Duration timeout(1.0 / 30);
+            tf_listener_.waitForTransform(cam_model_.tfFrame(), input.header.frame_id,
+                                          image_time_, timeout);
+            tf_listener_.lookupTransform(cam_model_.tfFrame(), input.header.frame_id,
+                                         image_time_, transform);
+        }
+        catch (tf::TransformException& ex) {
+            ROS_WARN("[draw_frames] TF exception:\n%s", ex.what());
+            return;
+        }
+
+        pcl::PointCloud<pcl::PointXYZRGB> camera_cloud;
+        pcl::fromROSMsg(input, camera_cloud);
+        pcl_ros::transformPointCloud(camera_cloud, camera_cloud, transform);
+
+        cv::Mat newimage(last_image_.size(), CV_8UC3);
+        newimage = cv::Scalar(0);
+
+        for (size_t i =0; i<camera_cloud.size(); i++) {
+            if (isnan (camera_cloud.points[i].x) || isnan (camera_cloud.points[i].y) || isnan (camera_cloud.points[i].z))
+                   continue;
+            cv::Point3d pt_cv(camera_cloud.points[i].x, camera_cloud.points[i].y, camera_cloud.points[i].z);
+            cv::Point2d uv = cam_model_.project3dToPixel(pt_cv);
+            int image_i = int(uv.y);
+            int image_j = int(uv.x);
+//            ROS_INFO("Accessing point (%d, %d)", image_i, image_j);
+            cv::Point3_<uchar>* p = newimage.ptr<cv::Point3_<uchar> >(image_i, image_j);
+
+
+            p->x = camera_cloud.points[i].r;
+            p->y = camera_cloud.points[i].g;
+            p->z = camera_cloud.points[i].b;
+//            uchar p0 = newimage.at<cv::Vec3b>(uv)[0];
+//            uchar p1 = newimage.at<cv::Vec3b>(uv)[1];
+//            uchar p2 = newimage.at<cv::Vec3b>(uv)[2];
+//            uchar p0 = camera_cloud.points[i].r;
+//            uchar p1 = camera_cloud.points[i].g;
+//            uchar p2 = camera_cloud.points[i].b;
+
+//            ROS_INFO("%d %d %d", p0, p1, p2);
+
+        }
+
+        std_msgs::Header newheader;
+        newheader.frame_id = cam_model_.tfFrame();
+        newheader.stamp = image_time_;
+        cv_bridge::CvImage cvimage(newheader, "rgb8", newimage);
+        imagepub_.publish(cvimage.toImageMsg());
+
+    }
+
+    void image_cb(const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info_msg) {
+
+        cv_bridge::CvImagePtr input_bridge;
+        try {
+            input_bridge = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
+            last_image_ = input_bridge->image;
+        }
+        catch (cv_bridge::Exception& ex){
+          ROS_ERROR("[draw_frames] Failed to convert image");
+          return;
+        }
+        cam_model_.fromCameraInfo(info_msg);
+        image_time_ = info_msg->header.stamp;
+        newimage_ = true;
+
+    }
+
+private:
+    ros::Subscriber pc_sub_;
+    ros::NodeHandle nh_;
+    tf::TransformListener tf_listener_;
+    image_transport::CameraSubscriber camerasub_;
+    image_transport::ImageTransport it_;
+    image_transport::Publisher imagepub_;
+
+    cv::Mat last_image_;
+    ros::Time image_time_;
+    image_geometry::PinholeCameraModel cam_model_;
+    bool newimage_;
+
+
+};
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "pointcloud_projector");
+    PointCloudProjector projector;
+    ros::spin();
+}
