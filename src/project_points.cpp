4f7821574e3089a217c088003673c7b086d55a7a
lorenzoriano <lorenzo.riano@berkeley.edu>
2013-02-20 18:16:43 -0800

    features and bug fixes

--- src/project_points.cpp
+++ src/project_points.cpp
@@ -12,19 +12,29 @@
 #include <cv_bridge/cv_bridge.h>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+#include <string>
 
 class PointCloudProjector {
 
 public:
-    PointCloudProjector() : nh_(), it_(nh_) {
-        pc_sub_ = nh_.subscribe("/camera/depth_registered/points", 1, &PointCloudProjector::cloud_cb, this);
-        camerasub_ = it_.subscribeCamera("/camera/rgb/image_rect_color", 1, &PointCloudProjector::image_cb, this);
+    PointCloudProjector() : nh_("~"), it_(nh_) {
+        std::string cloud_in;
+        nh_.param<std::string>("cloud_in", cloud_in, "/camera/depth_registered/points");
+        std::string camera_in;
+        nh_.param<std::string>("camera_in", camera_in, "/camera/rgb/image_rect_color");
+        ROS_INFO("Using input cloud of %s", cloud_in.c_str());
+        ROS_INFO("Using input images of %s", camera_in.c_str());
+
+        pc_sub_ = nh_.subscribe(cloud_in, 1, &PointCloudProjector::cloud_cb, this);
+        camerasub_ = it_.subscribeCamera(camera_in, 1, &PointCloudProjector::image_cb, this);
+
         imagepub_ = it_.advertise("image_out", 1);
 
         newimage_ = false;
     }
 
     void cloud_cb(const sensor_msgs::PointCloud2& input) {
+        ROS_INFO_ONCE("Pointcloud received");
 
         if (last_image_.empty()) {
             ROS_WARN("I don't have an image yet");
@@ -54,17 +64,21 @@ public:
         cv::Mat newimage(last_image_.size(), CV_8UC3);
         newimage = cv::Scalar(0);
 
+        int tot_points = 0;
+        int filled_points = 0;
         for (size_t i =0; i<camera_cloud.size(); i++) {
+            tot_points++;
             if (isnan (camera_cloud.points[i].x) || isnan (camera_cloud.points[i].y) || isnan (camera_cloud.points[i].z))
                    continue;
             cv::Point3d pt_cv(camera_cloud.points[i].x, camera_cloud.points[i].y, camera_cloud.points[i].z);
             cv::Point2d uv = cam_model_.project3dToPixel(pt_cv);
             int image_i = int(uv.y);
             int image_j = int(uv.x);
+            if (image_i >= newimage.rows || image_i < 0 || image_j >= newimage.cols || image_j < 0 ) {
+                continue;
+            }
 //            ROS_INFO("Accessing point (%d, %d)", image_i, image_j);
             cv::Point3_<uchar>* p = newimage.ptr<cv::Point3_<uchar> >(image_i, image_j);
-
-
             p->x = camera_cloud.points[i].r;
             p->y = camera_cloud.points[i].g;
             p->z = camera_cloud.points[i].b;
@@ -76,6 +90,7 @@ public:
 //            uchar p2 = camera_cloud.points[i].b;
 
 //            ROS_INFO("%d %d %d", p0, p1, p2);
+            filled_points++;
 
         }
 
@@ -84,10 +99,12 @@ public:
         newheader.stamp = image_time_;
         cv_bridge::CvImage cvimage(newheader, "rgb8", newimage);
         imagepub_.publish(cvimage.toImageMsg());
+        ROS_INFO("Filled %d points out of %d", filled_points, tot_points);
 
     }
 
     void image_cb(const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info_msg) {
+        ROS_INFO_ONCE("Image received");
 
         cv_bridge::CvImagePtr input_bridge;
         try {
